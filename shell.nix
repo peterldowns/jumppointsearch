d4950ed5cd1252df64837dae38cddcddb84766bd
Peter Downs <peter@pipe.com>
2022-11-06 17:00:20 -0500

    Clean up dependencies.

--- shell.nix
+++ shell.nix
@@ -1,12 +1,6 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> { } }:
 with pkgs;
 mkShell {
-  # Need to disable fortify hardening because GCC is not built with -oO,
-  # which means that if CGO_ENABLED=1 (which it is by default) then the golang
-  # debugger fails.
-  # see https://github.com/NixOS/nixpkgs/pull/12895/files
-  hardeningDisable = [ "fortify" ];
-
   buildInputs = [
     delve
     go-outline
@@ -16,6 +10,7 @@ mkShell {
     gopls
     gotools
     just
+    nixpkgs-fmt
   ];
 
   shellHook = ''
@@ -24,6 +19,7 @@ mkShell {
     # - dirname of the IN_LORRI_SHELL file when fired by lorri invocations
     shell_nix="''${IN_LORRI_SHELL:-$(pwd)/shell.nix}"
     workspace_root=$(dirname "$shell_nix")
+
     # The path to this repository
     export WORKSPACE_ROOT="$workspace_root"
     # Path to a folder that can hold build caches, etc. without them being
@@ -32,35 +28,29 @@ mkShell {
     # this in $HOME. Instead, we make it adjacent, as
     # `.pipe-$repositoryFolderName`, so that multiple checkouts of this repo in
     # the same folder will still each have their own caches.
-    export INSTALL_ROOT=$(dirname "$workspace_root")/.workspace-$(basename "$workspace_root")
+    export TOOLCHAIN_ROOT="$workspace_root/.toolchain"
 
     # Use a GOPATH local to this workspace so we get a portable
     # go environment that won't ever conflict with other repos or
     # other coding happening elsewhere.
     #
-    # We put the $GOPATH/$GOCACHE/$GOENV in $INSTALL_ROOT, which is
-    # adjacent to this repository root, to fix problems with vscode-go and
-    # gopls -- they don't like it when the $GOPATH is inside the repository
-    # being worked on.
-    #
-    # We also ensure the GOPATH's bin dir is on our PATH so tools
-    # installed with `go install` work.
-    #
-    # Long term, tools installed with `go install` can also be
-    # included directly as a nix package. Presently we're not
-    # sure which way is best-- perhaps both ways are fine :).
+    # We put the $GOPATH/$GOCACHE/$GOENV in $TOOLCHAIN_ROOT,
+    # and ensure that the GOPATH's bin dir is on our PATH so tools
+    # can be installed with `go install`.
     # 
     # Any tools installed explicitly with `go install` will take precedence
-    # over versions installed by Nix due to the ordering here. VSCode is
-    # configured to act this way already, explicitly, but this makes it so that
-    # the shell/environment in general has the same behavior. The downside to
-    # this is that if a user `go install`s a tool that we later update with
-    # nix, they will remain using the version that they `go install`ed. 
-    export GOCACHE="$INSTALL_ROOT/go/cache"
-    export GOENV="$INSTALL_ROOT/go/env"
-    export GOPATH="$INSTALL_ROOT/go/path"
-    export GOMODCACHE="$GOPATH/pkg/mod"
+    # over versions installed by Nix due to the ordering here.
     export GOROOT=
+    export GOCACHE="$TOOLCHAIN_ROOT/go/cache"
+    export GOENV="$TOOLCHAIN_ROOT/go/env"
+    export GOPATH="$TOOLCHAIN_ROOT/go/path"
+    export GOMODCACHE="$GOPATH/pkg/mod"
     export PATH=$(go env GOPATH)/bin:$PATH
   '';
+
+  # Need to disable fortify hardening because GCC is not built with -oO,
+  # which means that if CGO_ENABLED=1 (which it is by default) then the golang
+  # debugger fails.
+  # see https://github.com/NixOS/nixpkgs/pull/12895/files
+  hardeningDisable = [ "fortify" ];
 }
